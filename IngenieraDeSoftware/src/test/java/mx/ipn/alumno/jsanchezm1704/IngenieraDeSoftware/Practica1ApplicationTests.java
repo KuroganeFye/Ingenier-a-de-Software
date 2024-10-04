31a88230e5cc6cd8842da1bc093cdd8bf23ca204
Javier Sánchez Martínez <85339174+KuroganeFye@users.noreply.github.com>
2024-10-04 02:58:33 -0600

    modified:   .vscode/launch.json 	deleted:    CerradurasK.zip 	deleted:    IngenieraDeSoftware.zip 	modified:   IngenieraDeSoftware/pom.xml

---
+++ IngenieraDeSoftware/src/test/java/mx/ipn/alumno/jsanchezm1704/IngenieraDeSoftware/Practica1ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.ipn.alumno.jsanchezm1704.IngenieraDeSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
